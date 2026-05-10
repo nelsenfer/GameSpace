bb8d80c4f094462c85c0a5c9b8fc5863d47813f9
nels <nelsenfer17@gmail.com>
2026-05-11 00:07:43 +0700

    add ui blm fungsi

---
+++ Assets/script/HadesMenuHover.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class HadesMenuHover : MonoBehaviour, IPointerEnterHandler
+{
+    public Transform pointerIcon;
+    public GameObject darkHighlight;
+    public TextMeshProUGUI menuText;
+    public Color normalColor = new Color(0.7f, 0.7f, 0.7f);
+    public Color hoverColor = new Color(1f, 0.8f, 0.2f);
+
+    public bool isPlayButton = false;
+    private static HadesMenuHover currentSelected;
+
+    void Start()
+    {
+        if (isPlayButton)
+        {
+            SetActiveState(true);
+            currentSelected = this;
+        }
+        else
+        {
+            SetActiveState(false);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (currentSelected != null && currentSelected != this)
+        {
+            currentSelected.SetActiveState(false);
+        }
+
+        SetActiveState(true);
+        currentSelected = this;
+    }
+
+    private void SetActiveState(bool isActive)
+    {
+        darkHighlight.SetActive(isActive);
+        menuText.color = isActive ? hoverColor : normalColor;
+
+        if (isActive)
+        {
+            pointerIcon.gameObject.SetActive(true);
+            pointerIcon.position = new Vector3(pointerIcon.position.x, transform.position.y, pointerIcon.position.z);
+        }
+    }
+}
